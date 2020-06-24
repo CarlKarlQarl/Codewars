b1030a6c21671a9c88e50837fcdbe3df85f43853
CarlKarlQarl <karlmatthes@gmail.com>
2020-06-24 17:29:27 -0600

    Made frequency case insensitive, and that fixed it, wut

--- MostFrequentlyUsedWords.js
+++ MostFrequentlyUsedWords.js
@@ -1,18 +1,19 @@
 topThreeWords = text => {
-    let textArray = text
-      .split(".").join(" ")
-      .split(",").join(" ")
-      .split("//").join(" ")
-      .split(":").join(" ")
-      .split(" ")
-      .filter(word => word !== "" && word !== "'")
-    let frequency = {}
-    textArray.forEach(word => {
-      frequency[word] ? frequency[word] += 1 : frequency[word] = 1
-    })
-    let arrayFrequency = Object.entries(frequency).sort((a, b) => b[1] - a[1])
-    let arrayStripFrequency = arrayFrequency.map(pair => pair[0])
-    let top3 = arrayStripFrequency.slice(0, 3)
-    console.log(arrayFrequency)
-    return top3
+  let textArray = text
+    .split(".").join(" ")
+    .split(",").join(" ")
+    .split("//").join(" ")
+    .split(":").join(" ")
+    .split(" ")
+    .filter(word => word !== "" && word !== "'")
+  let frequency = {}
+  textArray.forEach(word => {
+    let lowercase = word.toLowerCase()
+    frequency[lowercase] ? frequency[lowercase] += 1 : frequency[lowercase] = 1
+  })
+  let arrayFrequency = Object.entries(frequency).sort((a, b) => b[1] - a[1])
+  let arrayStripFrequency = arrayFrequency.map(pair => pair[0])
+  let top3 = arrayStripFrequency.slice(0, 3)
+  console.log(arrayFrequency)
+  return top3
 }
