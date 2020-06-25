5fe46eaf2c7d74621f5c531a5ae4e70a1ebcc16a
CarlKarlQarl <karlmatthes@gmail.com>
2020-06-25 17:32:00 -0600

    Another possible solution, but it still loses too often. Going to refactor to have the code look for as many wins as possible first.

--- CanYouWinTheCodewar.js
+++ CanYouWinTheCodewar.js
@@ -4,27 +4,37 @@ codewarResult = (codewarrior, opponent) => {
     let sortedOpponent = opponent.sort((a, b) => b - a)
     let results = 0
     for (let i = 0; i < battle_length; i++) {
-      if(sum(codewarrior) < sum(opponent)){
-        let smallest = codewarrior.pop()
-        let largest = opponent.shift()
-        results--
-      } else {
-        let attacker = codewarrior.shift()
-        let defender = opponent.shift()
-        if (attacker > defender){
-          results++
+        if(sum(sortedCodewarrior) < sum(sortedOpponent)){
+            let attacker = sortedCodewarrior.pop()
+            let defender = sortedOpponent.shift()
+            console.log("Fight: ", attacker, defender)
+            results = results + determineVictor(attacker, defender)
+        } else {
+            let attacker = sortedCodewarrior.shift()
+            let defender = findBestFight(attacker, sortedOpponent)
+            console.log("Fight: ", attacker, defender)
+            let defender_index = sortedOpponent.indexOf(defender)
+            results = results + determineVictor(attacker, defender)
+            sortedOpponent.splice(defender_index, 1)
         }
-      }
     }
     if (results > 0){
-      return "Victory"
+        return "Victory"
     } else if (results < 0){
-      return "Defeat"
+        return "Defeat"
     } else {
-      return "Stalemate"
+        return "Stalemate"
     }
 }
   
-sum = array => array.reduce((num, memo) => num + memo)
-findLargest = array => array.reduce((num, memo) => memo > num ? memo : num)
-findSmallest = array => array.reduce((num, memo) => memo < num ? memo : num) 
+sum = array => array.reduce((num, memo) => num + memo, 0)
+findBestFight = (attacker, opponent) => opponent.find((num, index) => num <= attacker)
+determineVictor = (attacker, defender) => {
+    if (attacker > defender) {
+        return 1
+    } else if (attacker < defender) {
+        return -1
+    } else {
+        return 0
+    }
+}
